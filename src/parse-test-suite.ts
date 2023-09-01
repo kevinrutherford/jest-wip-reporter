b3f978864a4432448fbd85e1a65b86c40f2614e5
Kevin Rutherford <kevin@rutherford-software.com>
2023-09-01 14:17:03 +0100

    Break out another responsibility from parse-test-suite

--- src/parse-test-suite.ts
+++ src/parse-test-suite.ts
@@ -1,5 +1,7 @@
+/* eslint-disable no-param-reassign */
 import { classify } from './classify'
 import { SuiteReport, TestReport } from './suite-report'
+import { SuiteSummary } from './suite-summary'
 import { TestRun } from './test-run'
 
 export const toTestReport = (run: TestRun): TestReport => ({
@@ -7,6 +9,21 @@ export const toTestReport = (run: TestRun): TestReport => ({
   title: run.fullName,
   outcome: classify(run),
 })
+
+export const recordOn = (report: SuiteSummary) => (t: TestReport): TestReport => {
+  switch (t.outcome) {
+    case 'pass':
+      report.passedCount += 1
+      break
+    case 'wip':
+      report.wipTitles.push(t.title)
+      break
+    case 'fail':
+      report.failedCount += 1
+      break
+  }
+  return t
+}
 
 export const parseTestSuite = (suite: Array<TestRun>): SuiteReport => {
   const result: SuiteReport = {
@@ -16,19 +33,9 @@ export const parseTestSuite = (suite: Array<TestRun>): SuiteReport => {
     outcomes: [],
   }
   suite.forEach((currentRun) => {
-    const outcome = classify(currentRun)
-    switch (outcome) {
-      case 'pass':
-        result.passedCount += 1
-        break
-      case 'wip':
-        result.wipTitles.push(currentRun.fullName)
-        break
-      case 'fail':
-        result.failedCount += 1
-        break
-    }
-    result.outcomes.push(toTestReport(currentRun))
+    const t = toTestReport(currentRun)
+    recordOn(result)(t)
+    result.outcomes.push(t)
   })
   return result
 }
