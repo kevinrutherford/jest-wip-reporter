ab8933b1abb9f348cb8539269358e119caaa2e0f
Kevin Rutherford <kevin@rutherford-software.com>
2023-09-04 17:14:36 +0100

    Begin sketching tests for building the tree of suites

---
+++ test/add-to-report.test.ts
@@ -0,0 +1,7 @@
+describe('addToReport', () => {
+  describe('given a test with an ancestor', () => {
+    it.todo('adds a suite whose name is that of the ancestor')
+
+    it.todo('adds the test as a child of the suite')
+  })
+})
