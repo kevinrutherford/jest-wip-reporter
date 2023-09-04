acbd7010f482bb19e25f131c7cec0bddcea1ddc3
Kevin Rutherford <kevin@rutherford-software.com>
2023-09-04 17:24:00 +0100

    Begin implementing test setup

--- test/add-to-report.test.ts
+++ test/add-to-report.test.ts
@@ -1,6 +1,22 @@
+import * as FR from '../src/file-report'
+import { isTestReport } from '../src/report'
+import { arbitraryString } from './helpers'
+
 describe('addToReport', () => {
   describe('given a test with an ancestor', () => {
-    it.todo('adds a suite whose name is that of the ancestor')
+    const name = arbitraryString()
+    const fileReport = FR.constructTreeOfSuites([
+      {
+        _tag: 'test-report',
+        name,
+        fullyQualifiedName: arbitraryString(),
+        outcome: 'pass',
+      },
+    ])
+
+    it.failing('adds a suite whose name is that of the ancestor', () => {
+      expect(isTestReport(fileReport[0])).toBe(false)
+    })
 
     it.todo('adds the test as a child of the suite')
   })
