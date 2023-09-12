c47a1f239c452f21d1e6ded6a2cdc7e5a370fec4
Kevin Rutherford <kevin@rutherford-software.com>
2023-09-12 17:19:43 +0100

    Describe the next feature with tests

--- test/add-to-report.test.ts
+++ test/add-to-report.test.ts
@@ -2,7 +2,7 @@ import { pipe } from 'fp-ts/lib/function'
 import * as O from 'fp-ts/Option'
 import * as RA from 'fp-ts/ReadonlyArray'
 import {
-  isSuiteReport, isTestReport, Report, SuiteReport, TestReport,
+  isSuiteReport, isTestReport, Report, SuiteReport, TestOutcome, TestReport,
 } from '../src/report'
 import { arbitraryString } from './helpers'
 import * as progressTree from '../src/progress-tree'
@@ -48,35 +48,51 @@ describe('addToReport', () => {
   })
 
   describe('given two tests with the same single ancestor', () => {
-    const ancestorName = arbitraryString()
-    const name1 = arbitraryString()
-    const name2 = arbitraryString()
-    const suiteNode = pipe(
-      [
-        {
-          _tag: 'test-report',
-          name: name1,
-          fullyQualifiedName: arbitraryString(),
-          ancestorNames: [ancestorName],
-          outcome: 'pass',
-        },
-        {
-          _tag: 'test-report',
-          name: name2,
-          fullyQualifiedName: arbitraryString(),
-          ancestorNames: [ancestorName],
-          outcome: 'pass',
-        },
-      ],
-      constructTreeOfSuites,
-      RA.head,
-      O.getOrElseW(() => { throw new Error('Expected at least one node in the tree') }),
-      O.fromPredicate(isSuiteReport),
-      O.getOrElseW(() => { throw new Error('Expected the root node to be a suite') }),
-    )
+    describe.each([
+      ['pass', 'pass', 'pass'],
+      ['pass', 'wip', 'wip'],
+      ['pass', 'fail', 'fail'],
+      ['wip', 'pass', 'wip'],
+      ['wip', 'wip', 'wip'],
+      ['wip', 'fail', 'fail'],
+      ['fail', 'pass', 'fail'],
+      ['fail', 'wip', 'fail'],
+      ['fail', 'fail', 'fail'],
+    ])('when the test outcomes are %s and %s', (outcome1, outcome2, expectedAncestorOutcome) => {
+      const ancestorName = arbitraryString()
+      const name1 = arbitraryString()
+      const name2 = arbitraryString()
+      const suiteNode = pipe(
+        [
+          {
+            _tag: 'test-report',
+            name: name1,
+            fullyQualifiedName: arbitraryString(),
+            ancestorNames: [ancestorName],
+            outcome: outcome1 as TestOutcome,
+          },
+          {
+            _tag: 'test-report',
+            name: name2,
+            fullyQualifiedName: arbitraryString(),
+            ancestorNames: [ancestorName],
+            outcome: outcome2 as TestOutcome,
+          },
+        ],
+        constructTreeOfSuites,
+        RA.head,
+        O.getOrElseW(() => { throw new Error('Expected at least one node in the tree') }),
+        O.fromPredicate(isSuiteReport),
+        O.getOrElseW(() => { throw new Error('Expected the root node to be a suite') }),
+      )
 
-    it('adds both tests as children of the same suite node', () => {
-      expect(suiteNode.children).toHaveLength(2)
+      it('adds both tests as children of the same suite node', () => {
+        expect(suiteNode.children).toHaveLength(2)
+      })
+
+      it.skip(`sets the outcome of the suite to ${expectedAncestorOutcome}`, () => {
+        expect(suiteNode.outcome).toBe(expectedAncestorOutcome)
+      })
     })
   })
 
