55040b87b06cc642b523b4b6f5f87e4ec5827d6c
Kevin Rutherford <kevin@rutherford-software.com>
2023-09-18 08:56:09 +0100

    Add more test coverage

--- test/progress/progress-tree.test.ts
+++ test/progress/progress-tree.test.ts
@@ -5,10 +5,18 @@ import { arbitraryString } from '../helpers'
 
 describe('progress-tree', () => {
   describe('rendering a pass test', () => {
-    it('does not render the standard dot', () => {
+    const name = arbitraryString()
+    const t: TestReport = {
+      _tag: 'test-report',
+      name,
+      fullyQualifiedName: arbitraryString(),
+      ancestorNames: [],
+      outcome: 'pass',
+    }
+
+    it('includes the name of the test', () => {
       let renderedLine = ''
       const write = (s: string) => { renderedLine += s }
-      const name = arbitraryString()
       const config: Config = {
         write,
         pens: {
@@ -17,16 +25,24 @@ describe('progress-tree', () => {
           fail: write,
         },
       }
-      const t: TestReport = {
-        _tag: 'test-report',
-        name,
-        fullyQualifiedName: arbitraryString(),
-        ancestorNames: [],
-        outcome: 'pass',
-      }
       renderTestReport(config, 0)(t)
 
       expect(renderedLine).toContain(name)
+    })
+
+    it('does not render the standard dot', () => {
+      let renderedLine = ''
+      const write = (s: string) => { renderedLine += s }
+      const config: Config = {
+        write,
+        pens: {
+          pass: write,
+          wip: write,
+          fail: write,
+        },
+      }
+      renderTestReport(config, 0)(t)
+
       expect(renderedLine.startsWith('.')).toBe(false)
     })
   })
