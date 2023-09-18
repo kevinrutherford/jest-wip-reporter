636db7cc297a312ac5a6b744158d977963086151
Kevin Rutherford <kevin@rutherford-software.com>
2023-09-18 08:21:51 +0100

    Confirm that progress tree reporting uses a different dot

--- test/progress/progress-tree.test.ts
+++ test/progress/progress-tree.test.ts
@@ -1,10 +1,32 @@
+import { Config } from '../../src/config'
+import { renderTestReport } from '../../src/progress/progress-tree'
+import { TestReport } from '../../src/report'
 import { arbitraryString } from '../helpers'
 
 describe('progress-tree', () => {
   describe('rendering a pass test', () => {
     it('does not render the standard dot', () => {
-      const renderedLine = arbitraryString()
+      let renderedLine = ''
+      const write = (s: string) => { renderedLine += s }
+      const name = arbitraryString()
+      const config: Config = {
+        write,
+        pens: {
+          pass: write,
+          wip: write,
+          fail: write,
+        },
+      }
+      const t: TestReport = {
+        _tag: 'test-report',
+        name,
+        fullyQualifiedName: arbitraryString(),
+        ancestorNames: [],
+        outcome: 'pass',
+      }
+      renderTestReport(config, 0)(t)
 
+      expect(renderedLine).toContain(name)
       expect(renderedLine.startsWith('.')).toBe(false)
     })
   })
