73b5241e56b54e2c7a40c764ead0a31c62eba60d
Kevin Rutherford <kevin@rutherford-software.com>
2023-09-26 08:17:56 +0100

    Express the requirement with a failing test #41

---
+++ test/summary/render.test.ts
@@ -0,0 +1,29 @@
+import { Config } from '../../src/config'
+import { render } from '../../src/summary/render'
+
+describe('render', () => {
+  describe('when there were no tests', () => {
+    it.failing('reports that 0 tests were run', () => {
+      let output = ''
+      const write = (s: string) => { output += s }
+      const config: Config = {
+        write,
+        pens: {
+          pass: write,
+          wip: write,
+          fail: write,
+        },
+      }
+      render({
+        passedCount: 0,
+        failedCount: 0,
+        wipTitles: [],
+      }, config)({
+        startTime: 0,
+        testResults: [],
+      })
+
+      expect(output).toContain('0 run')
+    })
+  })
+})
